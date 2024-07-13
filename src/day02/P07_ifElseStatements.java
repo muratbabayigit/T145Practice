4dde8024b32a56487e8d23e93267ddd5169e9087
Babayiğit <babayigit@gmail.com>
2024-07-13 22:24:26 +0300

    merhaba

--- src/day02/P07_ifElseStatements.java
+++ src/day02/P07_ifElseStatements.java
@@ -1,5 +1,7 @@
 package day02;
 
+import java.util.Scanner;
+
 public class P07_ifElseStatements {
     public static void main(String[] args) {
         /* Problem tanimi : Kulanicidan aracacinin hizini aliniz Trafik cezasinin
@@ -10,13 +12,43 @@ public class P07_ifElseStatements {
          * Eger Hiziniz 94'den daha fazla ise: Ceza 500 $'dir.
          * Eger kullanicinin ehliyeti yoksa cezaya 200 $ eklenir.
          * orn: Hiziniz 77 iken cezaniz 150$'dir ama ehliyetiniz yoksa ceza 350 $ olur.
-         * -----------------------------------------
-         * Orn;
-         * currentSpeed(Hiziniz) 87 ve isDriverLicenceAvailable(Ehliyeti var mi) = true;
-         * sonuc 320 olmalidir.
-         * currentSpeed(Hiziniz) 65 ve isDriverLicenceAvailable(Ehliyeti var mı?) =
-         * false; sonuc 300 olmalidir.
+         * ----------------------------------------
          */
+
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Ehliyet Var mı?:  (E/H)");
+        char isDrivingLicence=scan.next().toUpperCase().charAt(0);
+        System.out.print("Hız değeri: ");
+        int velocity= scan.nextInt();
+
+        if (isDrivingLicence=='E'){
+            if (velocity<55){
+                System.out.println("Hız limitlerine uyduğunuz için teşekkür ederiz...");
+            } else if (velocity>=55 && velocity<=74 ){
+                System.out.println("Hız sınırını aştınız ceza tutarınız: 100$");
+            } else if (velocity>=75 && velocity<=84) {
+                System.out.println("Hız sınırını aştınız ceza tutarınız: 150$");
+            } else if (velocity>=85 && velocity<=94) {
+                System.out.println("Hız sınırını aştınız ceza tutarınız: 320$");
+            }else if(velocity>94){
+                System.out.println("Hız sınırını aştınız ceza tutarınız: 500$");
+            }
+        }else if(isDrivingLicence=='H'){
+            if (velocity<55) {
+                System.out.println("Ehliyetsiz araç kullanıyorsunuz cezanız 200$. Lütfen araçtan ininiz");
+            }else if (velocity>=55 && velocity<=74 ){
+                System.out.println("Hız sınırını aştınız ceza tutarınız: 300$");
+            } else if (velocity>=75 && velocity<=84) {
+                System.out.println("Hız sınırını aştınız ceza tutarınız: 350$");
+            } else if (velocity>=85 && velocity<=94) {
+                System.out.println("Hız sınırını aştınız ceza tutarınız: 520$");
+            }else if(velocity>94){
+                System.out.println("Hız sınırını aştınız ceza tutarınız: 700$");
+            }
+
+        }else{
+            System.out.println("Ehliyet bilgisi yanlış girildi");
+        }
 
 
     }
